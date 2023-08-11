8e184517e5f5035657df7a0047ef63a06f6514e8
Lale-git <141859286+Lale-git@users.noreply.github.com>
2023-08-11 16:22:21 +0530

    Update srteamlit_app.py

--- srteamlit_app.py
+++ srteamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('This is My New Healthy Diner!!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spanish and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg Menu')
