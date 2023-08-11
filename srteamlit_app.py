8a13e5c3a703c9f3d6220eb91399dae730c744d0
Lale-git <141859286+Lale-git@users.noreply.github.com>
2023-08-11 16:27:05 +0530

    Update srteamlit_app.py
    Added breakfast Menu header and 4 items.

--- srteamlit_app.py
+++ srteamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('This is My New Healthy Diner!!')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spanish and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spanish and Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg Menu')
+streamlit.text('🥑🍞 Avocado Toast')
